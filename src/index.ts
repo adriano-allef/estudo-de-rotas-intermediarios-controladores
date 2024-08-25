eedeb353da9038b35f7d13f11f5a615564e24524
Adriano Allef Pereira <adrianopereira951@outlook.com>
2024-08-25 14:10:14 -0300

    Parametros de rotas e paramentros de consulta

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,49 @@ import express from 'express'
 
 const servidor = express()
 
-servidor.get('/', (req, res) => {
-    return res.send('Servidor Funcionando!')
+servidor.get('/produtos/:item', (req, res) => {
+    console.log(req.params)
+    return res.send('O nodemon está configurado com o typescript')
+} )
+
+const pessoas = [
+    {nome: 'Adriano', email: 'adriano@email.com'},
+    {nome: 'João', email: 'joao@email.com'},
+    {nome: 'Maria', email: 'maria@email.com'}, 
+]
+
+servidor.get('/usuarios/:email', (req, res) => {
+    const { email } = req.params
+
+    const pessoa = pessoas.find((item) =>{
+        return item.email === email
+    } )
+
+    if (!pessoa){
+        return res.send('Pessoa não encontrada!')
+    }
+
+    return res.send(pessoa)
+} )
+
+servidor.get('/usuarios', (req, res) => {
+    const { email } = req.query
+
+    if (!email) {
+        return res.send('parametro não informado')
+    }
+
+
+    console.log(req.query)
+    const pessoa = pessoas.find((item) =>{
+        return item.email === email
+    } )
+
+    if (!pessoa) {
+        return res.send('Pessoa não encontrada!')
+    }
+
+    return res.send('OK')
 } )
 
 servidor.listen(process.env.PORT)
